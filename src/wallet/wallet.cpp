93cdf588cf9d4eed75a38abed8e6758d4f894a0f
Tom Harding <tomh@thinlink.com>
2015-03-11 16:48:53 -0700

    Implement CTransaction::IsEquivalentTo(...)
    Define CTransaction::IsEquivalentTo(const CTransaction& tx)
    
    True if only scriptSigs are different.  In other words, true if
    the two transactions are malleability clones.  In other words,
    true if the two transactions have the same effect on the
    outside universe.
    
    In the wallet, only SyncMetaData for equivalent transactions.

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -536,6 +536,7 @@ void CWallet::SyncMetaData(pair<typename TxSpendMap<T>::iterator, typename TxSpe
         const uint256& hash = it->second;
         CWalletTx* copyTo = &mapWallet[hash];
         if (copyFrom == copyTo) continue;
+        if (!copyFrom->IsEquivalentTo(*copyTo)) continue;
         copyTo->mapValue = copyFrom->mapValue;
         // mapNoteData not copied on purpose
         // (it is always set correctly for each CWalletTx)
